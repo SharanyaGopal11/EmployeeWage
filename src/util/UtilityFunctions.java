e24b788f3c71db65b051d3c5ee519e5fa30894ee
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-07-04 12:51:05 +0530

    calcualate employee wage for total hours or days

--- src/util/UtilityFunctions.java
+++ src/util/UtilityFunctions.java
@@ -4,19 +4,22 @@ public class UtilityFunctions {
 	public void isPresent() {
 		 int wage =0, wageDay = 0;
 		 int fullTimeHours = 8, partTimeHours = 4, wagePerHour = 20;
+		 int totalHours = 100, totalDays = 20, passedHours = 0, passedDays = 0;
 		 
-		 for ( int i = 0; i< 20; i++){
+		 while(passedHours <= totalHours && passedDays <= totalDays){
 			 int isPresent = (int)(Math.floor(Math.random() * 10)% 3);
-			 if (isPresent == 1){
+			 if(isPresent == 1){
 				 wageDay = fullTimeHours * wagePerHour;
+				 passedHours = passedHours + fullTimeHours;
 			 }
-			 if (isPresent == 2){
+			 else if(isPresent == 2){
 				 wageDay = partTimeHours * wagePerHour;
+				 passedHours = passedHours + partTimeHours;
 			 }
-			 wage = wage + wageDay;
+			 wage = wageDay + wage;
 		 }
 		 
-		 System.out.print("Employee one month wage is "+wage);
+		 System.out.print("Employee wage is "+wage);
 			 
 	 }
 
