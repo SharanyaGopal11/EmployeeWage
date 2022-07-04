9b69297886e9798f0f9dd33b47313866773a07e1
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-07-04 12:43:46 +0530

    To calculate employee wage for a month

--- src/util/UtilityFunctions.java
+++ src/util/UtilityFunctions.java
@@ -2,26 +2,21 @@ package util;
 
 public class UtilityFunctions {
 	public void isPresent() {
-		 int isPresent = (int)(Math.floor(Math.random() * 10)% 3);
-		 int wage =0;
+		 int wage =0, wageDay = 0;
 		 int fullTimeHours = 8, partTimeHours = 4, wagePerHour = 20;
 		 
-		 switch (isPresent){
-		 case 1 :
-			 wage = fullTimeHours * wagePerHour ;
-			 System.out.println("Employee is present and wage is "+wage);
-			 break;
-		 case 2 :
-			 wage = partTimeHours * wagePerHour ;
-			 System.out.println("Employee is present part time and wage is "+wage);
-			 break;
-		 case 0 :
-			 System.out.println("Employee is absent and wage is "+wage);
-			 break;
-		default :
-			System.out.print("its default case");
+		 for ( int i = 0; i< 20; i++){
+			 int isPresent = (int)(Math.floor(Math.random() * 10)% 3);
+			 if (isPresent == 1){
+				 wageDay = fullTimeHours * wagePerHour;
+			 }
+			 if (isPresent == 2){
+				 wageDay = partTimeHours * wagePerHour;
+			 }
+			 wage = wage + wageDay;
 		 }
 		 
+		 System.out.print("Employee one month wage is "+wage);
 			 
 	 }
 
