c999e38a39bc3bc56013e09489fafe1bfaf12396
Sharanya Gopal <sharanyagopal11@gmail.com>
2022-07-04 12:37:41 +0530

    calculating employee wage using switch statement

--- src/util/UtilityFunctions.java
+++ src/util/UtilityFunctions.java
@@ -2,35 +2,27 @@ package util;
 
 public class UtilityFunctions {
 	public void isPresent() {
-		int isPresent = (int)(Math.floor(Math.random() * 10)% 3);
+		 int isPresent = (int)(Math.floor(Math.random() * 10)% 3);
 		 int wage =0;
-		 int fullTime = 1, partTime = 2 , absent = 0;
 		 int fullTimeHours = 8, partTimeHours = 4, wagePerHour = 20;
 		 
+		 switch (isPresent){
+		 case 1 :
+			 wage = fullTimeHours * wagePerHour ;
+			 System.out.println("Employee is present and wage is "+wage);
+			 break;
+		 case 2 :
+			 wage = partTimeHours * wagePerHour ;
+			 System.out.println("Employee is present part time and wage is "+wage);
+			 break;
+		 case 0 :
+			 System.out.println("Employee is absent and wage is "+wage);
+			 break;
+		default :
+			System.out.print("its default case");
+		 }
 		 
-		 if (isPresent == fullTime ){
-				/**
-				 Employee is full time present if the value is 1
-				 and his wage is calculated accordingly
-				 */
-			 wage = fullTimeHours * wagePerHour;
-			 System.out.println("Employee is present and his wage is "+wage); 
-		 }
-		 else if (isPresent == partTime ){
-				/**
-			 Employee is part time present if the value is 2
-			 and his wage is calculated accordingly
-			 */
-			 wage = partTimeHours * wagePerHour;
-			 System.out.println("Employee is part time and his wage is "+wage);   // Employee is absent if the value is less than 0.5
-		 }
-		 else if (isPresent == absent ){
-				/**
-			 Employee is absent if the value is 0
-			 and his wage is 0
-			 */
-			 System.out.println("Employee is absent and his wage is "+wage);
-		 }
+			 
 	 }
 
 	
